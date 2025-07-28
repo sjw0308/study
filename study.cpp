f14b1b3c00b5608e782ccdec5a3471707b41eca5
sjw0308 <98965292+sjw0308@users.noreply.github.com>
2025-07-28 05:53:36 +0000

    10807

--- study.cpp
+++ study.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
 int main(){
-    std::cout << "Hello World" << std::endl;
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+    
+    int a, arr[100], b, cnt = 0;
+    cin >> a;
+    for (int i = 0; i < a; i++){
+        cin >> arr[i];
+    }
+    cin >> b;
+    for (int i = 0; i < a; i++){
+        if (arr[i] == b) cnt++;
+    }
+
+    cout << cnt << endl;
+
     return 0;
 }
