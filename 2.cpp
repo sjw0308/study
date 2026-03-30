dae64b9af1f7c5c61b5d9bf6fb25a29cc6b45e72
jiwoo <jiwoo0308@postech.ac.kr>
2026-03-30 15:48:50 +0900

    202603301548

--- 2.cpp
+++ 2.cpp
@@ -7,52 +7,49 @@
 
 using namespace std;
 
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-int b[105][105];
-bool vi[105][105];
+// int dx[4] = {1,0,-1,0};
+// int dy[4] = {0,1,0,-1};
+// int b[105][105];
+// int vi[105][105];
+
+int b[100005];
+int vi[200005];
+int d[] = {1,-1};
+int n, m;
+
+
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    int n,m,k,cnt=0;
-    cin >> n >> m >> k;
-    vector<int> re={};
-    queue<pair<int,int>> q;
-    for (int i=0; i<k; i++){
-        int lx, ly, rx, ry;
-        cin >> ly >> lx >> ry >> rx;
-        for(int x=lx; x<rx; x++){
-            for(int y=ly; y<ry; y++) b[x][y] = 1;
-        }
+    int k;
+    cin >> n >> k;
+    deque<int> dq;
+    fill(vi,vi+200005,-1);
+    if(k<=n) {
+        cout << n-k;
+        return 0;
     }
+    vi[n] = 0;
+    dq.push_back(n);
+    while(!dq.empty()){
+        int cur = dq.front();
+        if (cur == k) break;
+        dq.pop_back();
+        int nx;
+        for(int i =0;i<2;i++){
+            nx = cur + d[i];
+            if(nx <0 || nx>=200005) continue;
+            if(vi[nx]!=-1) continue;
+            vi[nx] = vi[cur]+1;
+            dq.push_back(nx);
+        }
+        nx = cur * 2;
+        if(nx < 0 || nx >= 200005) continue;
+        if(vi[nx] != -1) continue;
+        vi[nx] = vi[cur];
+        dq.push_front(nx);
+    }
+    cout << vi[k];
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(!vi[i][j] && !b[i][j]){
-                cnt++;
-                vi[i][j] = 1;
-                int s = 1;
-                q.push({i,j});
-                while(!q.empty()){
-                    auto cur = q.front();
-                    q.pop();
-                    for(int dir=0; dir<4; dir++){
-                        int nx = cur.x + dx[dir];
-                        int ny = cur.y + dy[dir];
-                        if(nx<0 || nx>=n || ny < 0|| ny>=m) continue;
-                        if(vi[nx][ny] || b[nx][ny]) continue;
-                        s++;
-                        vi[nx][ny] = 1;
-                        q.push({nx,ny});
-                    }
-                }
-                re.push_back(s);
-            }
-        }
-    }
-    cout << cnt << '\n';
-    sort(re.begin(), re.end());
-    for(int i: re) cout << i << ' ';
 }
