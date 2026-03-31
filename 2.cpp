fec7e042fc5e3c7ee7940b140e8889aeb38e192f
jiwoo <jiwoo0308@postech.ac.kr>
2026-04-01 01:35:21 +0900

    202604010135

--- 2.cpp
+++ 2.cpp
@@ -9,47 +9,70 @@ using namespace std;
 
 // int dx[4] = {1,0,-1,0};
 // int dy[4] = {0,1,0,-1};
-// int b[105][105];
-// int vi[105][105];
+// int  b[205][205];
+// int vi[205][205][31];
+// int n, m;
 
-int b[100005];
-int vi[200005];
-int d[] = {1,-1};
-int n, m;
+int vi[2][200005];
+int d[2] = {1,-1};
 
 
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int k;
+    
+    int n,k;
     cin >> n >> k;
-    deque<int> dq;
-    fill(vi,vi+200005,-1);
-    if(k<=n) {
-        cout << n-k;
+    if (k <= n){
+        cout << n-k << '\n';
+        for(int i=n; i>=k; i--) cout << i << ' ';
         return 0;
     }
-    vi[n] = 0;
-    dq.push_back(n);
-    while(!dq.empty()){
-        int cur = dq.front();
-        if (cur == k) break;
-        dq.pop_back();
+    fill(vi[1], vi[1]+200005, -1);
+    fill(vi[0], vi[0]+200005, -1);
+    vi[0][n] = 0;
+    vi[1][n] = 0;
+    queue<int> q;
+    q.push(n);
+    int find = 0;
+    while(!q.empty()){
+        if(find) break;
+        int cur = q.front();
+        q.pop();
         int nx;
-        for(int i =0;i<2;i++){
-            nx = cur + d[i];
-            if(nx <0 || nx>=200005) continue;
-            if(vi[nx]!=-1) continue;
-            vi[nx] = vi[cur]+1;
-            dq.push_back(nx);
+        for(int dir=0; dir<2; dir++){
+            nx = cur + d[dir];
+            if(nx < 0 || nx > 200001) continue;
+            if(vi[0][nx] > -1) continue;
+            vi[0][nx] = vi[0][cur]+1;
+            vi[1][nx] = cur;
+            if(nx == k){
+                find = 1;
+                break;
+            }
+            q.push(nx);
         }
-        nx = cur * 2;
-        if(nx < 0 || nx >= 200005) continue;
-        if(vi[nx] != -1) continue;
-        vi[nx] = vi[cur];
-        dq.push_front(nx);
+        nx = 2*cur;
+        if(nx < 0 || nx > 200001) continue;
+        if(vi[0][nx] > -1) continue;
+        vi[0][nx] = vi[0][cur]+1;
+        vi[1][nx] = cur;
+        if(nx == k){
+            find = 1;
+            break;
+        }
+        q.push(nx);
     }
-    cout << vi[k];
-
+    int cur = k;
+    vector<int> ans;
+    cout << vi[0][k] << '\n';
+    while(cur != n){
+        ans.push_back(cur);
+        cur = vi[1][cur];
+    }
+    ans.push_back(n);
+    for(int i = ans.size()-1; i >= 0; i--){
+        cout << ans[i] << ' ';
+    }
 }
