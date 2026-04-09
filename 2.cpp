2995f9efb0134164a1522c8aa5fee5f4d6b9b1f3
jiwoo <jiwoo0308@postech.ac.kr>
2026-04-10 02:00:21 +0900

    202604100200

--- 2.cpp
+++ 2.cpp
@@ -7,23 +7,19 @@
 
 using namespace std;
 
-int n, m, o[10];
-bool isused[10];
+int n, m;
+char c[20];
 
-void func(int k, vector<int> arr){
-    if(k==m){
-        for(int i=0; i<m; ++i) cout << arr[o[i]] << ' ';
-        cout << '\n';
-        return;
+bool check(vector<char> c){
+    bool a=0, b = 0;
+    int cnt = 0;
+    for(int i=0; i<n; ++i) {
+        bool tmp = (c[i]=='a' || c[i]=='e'|| c[i]=='i'|| c[i]=='o'|| c[i]=='u');
+        a = a || tmp;
+        if(!tmp) ++cnt;
     }
-    for(int i=0; i<n; ++i){
-        if(!isused[i]){
-            o[k] = i;
-            isused[i] = true;
-            func(k+1, arr);
-            isused[i] = false;
-        }
-    }
+    b = (cnt>=2);
+    return a && b;
 }
 
 int main() {
@@ -31,8 +27,22 @@ int main() {
     cin.tie(NULL);
 
     cin >> n >> m;
-    vector<int> arr(n);
-    for(int i=0; i<n; ++i) cin >> arr[i];
-    sort(arr.begin(), arr.end());
-    func(0,arr);
+    for(int i=0; i<m; ++i) cin >> c[i];
+    sort(c, c+m);
+
+    vector<int> v(m,1);
+    for(int i=0; i<n; ++i) v[i] = 0;
+
+    vector<char> tmp;
+
+    do{
+        tmp.clear();
+        for(int i=0; i<m; ++i) if(!v[i]) tmp.push_back(c[i]);
+        if(check(tmp)) {
+            for(char ch : tmp) cout << ch;
+            cout << "\n";
+        }
+    }while(next_permutation(v.begin(), v.end()));
+
+
 }
